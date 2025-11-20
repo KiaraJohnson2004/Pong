8f81d73c306c05dbeadbe0e2c111d205dd3a9b7b
KiaraJohnson2004 <princesskiarajohnson04@gmail.com>
2025-11-20 16:28:42 -0500

    Removed unnecessary library

--- pongClient.py
+++ pongClient.py
@@ -6,7 +6,7 @@
 # Misc:                     <Not Required.  Anything else you might want to include>
 # =================================================================================================
 
-from telnetlib import GA
+
 import pygame
 import tkinter as tk
 import sys
